a28ec576ef134aeda45f6c5311b3f34db44217eb
Annazubakha <korepovaanna@gmail.com>
2023-11-09 13:33:02 +0100

    exs1

---
+++ js/module_1/module-1-lesson-1.js
@@ -0,0 +1,10 @@
+// https://marketplace.visualstudio.com/items?itemName=akamud.vscode-javascript-snippet-pack
+
+
+console.log("q");
+// null, 0, undefined, false, "" , NaN
+
+//? Перевірте чи парні числа 15, 20, 45, 30.
+const isEven = !(15 % 2) 
+console.log(isEven);
+

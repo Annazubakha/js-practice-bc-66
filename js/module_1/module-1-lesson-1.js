c71980fc4f0f88b1a1b85dea1c466c33c426b7f7
B.O. Vdovych <123995120+Kianor1@users.noreply.github.com>
2023-11-09 14:58:52 +0200

    task2

--- js/module_1/module-1-lesson-1.js
+++ js/module_1/module-1-lesson-1.js
@@ -1,10 +1,27 @@
 // https://marketplace.visualstudio.com/items?itemName=akamud.vscode-javascript-snippet-pack
 
-
-console.log("q");
+// console.log("q");
 // null, 0, undefined, false, "" , NaN
 
 //? Перевірте чи парні числа 15, 20, 45, 30.
-const isEven = !(15 % 2) 
-console.log(isEven);
+// const isEven = !(15 % 2)
+// console.log(isEven);
 
+/*
+  ? Дано 90 хвилин, дізнайтесь скільки в даному числі міститься годин і хвилин.
+  ? Результат виведіть у консоль.
+*/
+//70 === 01:10
+//90 === 01:30
+
+const time = 90;
+const hours = Math.floor(time / 60);
+const updHours = (hours + "").padStart(2, "0");
+
+// console.log(updHours);
+
+const min = time % 60;
+console.log(min);
+// const min = time - hours * 60;
+
+console.log("10".padStart(2, "0"));

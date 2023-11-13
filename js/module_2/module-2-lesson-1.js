f2209c95e503bed5bf179506033d9398bf757081
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-11-13 14:52:44 +0200

    add task2

--- js/module_2/module-2-lesson-1.js
+++ js/module_2/module-2-lesson-1.js
@@ -1,14 +1,39 @@
-
-const fruits = ['🍎', '🍑', '🍇', '🍑', '🍌', '🍋', '🍑'];
+// const fruits = ['🍎', '🍑', '🍇', '🍑', '🍌', '🍋', '🍑'];
 // const index=fruits.indexOf('🍇')
 // for (let i = 0; i < fruits.length; i++) {
 //     if(i===index){continue}
 //     console.log(fruits[i])
 // }
 
-for (let i = 0; i < fruits.length; i++) {
-    if (fruits[i] === '🍇') {
-        continue
-    }
-    console.log(fruits[i])
-}
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruits[i] === '🍇') {
+//         continue
+//     }
+//     console.log(fruits[i])
+// }
+
+/*
+? Створіть масив genres з елементами Jazz і Blues.
+? Додайте "rock'n'roll" до кінця.
+? Виведіть у консоль перший елемент масиву.
+? Виведіть у консоль останній елемент масиву. Код має працювати для масиву довільної довжини.
+? Видаліть перший елемент та виведіть його в консоль.
+? Вставте "Country" та "Reggy" на початок масиву.
+ */
+
+const genres = ['Jazz', 'Blues'];
+const newArr = genres.splice(genres.length, 0, "rock'n'roll");
+// genres[genres.length] = "rock'n'roll";
+
+// const newArr = genres.push("rock'n'roll");
+
+// console.log(genres[genres.length - 1]);
+
+// const firstElem = genres.shift();
+// const firstElem = genres.splice(0, 1);
+// console.log(firstElem[0]);
+// genres.unshift('Country', 'Reggy');
+genres.splice(0, 0, 'Country', 'Reggy');
+
+console.log(genres);
+// console.log(newArr);

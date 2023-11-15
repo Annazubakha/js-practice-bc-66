7ce759666543ceca6bfa4b3d3ddee0d6cfb68e0e
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2023-11-15 14:11:34 +0100

    m3-l5

--- js/module_3/module-3-lesson-1.js
+++ js/module_3/module-3-lesson-1.js
@@ -31,5 +31,118 @@
 // console.log(calcAverage(1, 2, 3, "st", 4, 5, 6, "sd", 7, 8, 9, 0));
 // console.log(Number.isNaN(Number(1)));
 // console.log(Number.isNaN(Number("2kl")));
-console.log(typeof 1);
-console.log(typeof typeof 1);
+// console.log(typeof 1);
+// console.log(typeof typeof 1);
+
+const filmoteka = [
+  {
+    id: 1,
+    title: "Duna",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 10,
+    actors: [
+      "Amelia Gibson",
+      "Cameron Clayton",
+      "Viola Vasquez",
+      "Lula Collins",
+      "Victor Nichols",
+    ],
+    adult: false,
+    image: "https://via.placeholder.com/640/480",
+  },
+  {
+    id: 2,
+    title: "Venom",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 8,
+    actors: [
+      "Inez Greer",
+      "Barry Curtis",
+      "Blanche Rios",
+      "Glen Perry",
+      "Logan Powers",
+    ],
+    adult: true,
+    image: "https://via.placeholder.com/640/480",
+  },
+  {
+    id: 3,
+    title: "Luca",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 9,
+    actors: [
+      "Marguerite Gibbs",
+      "Lora Alvarez",
+      "Jorge Simpson",
+      "Thomas Hall",
+      "Darrell Hunter",
+    ],
+    adult: false,
+  },
+  {
+    id: 4,
+    title: "Free Guy",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 6,
+    actors: [
+      "Calvin Andrews",
+      "Mamie Myers",
+      "Madge Townsend",
+      "Ralph Kim",
+      "Jorge Reese",
+    ],
+    adult: false,
+    image: "https://via.placeholder.com/640/480",
+  },
+  {
+    id: 5,
+    title: "Saw",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 7,
+    actors: [
+      "Adele Marsh",
+      "Melvin Burgess",
+      "Jesus Reese",
+      "Harriet Moreno",
+      "Curtis Cox",
+    ],
+    adult: true,
+  },
+  {
+    id: 6,
+    title: "Duna 2",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    rating: 10,
+    actors: [
+      "Amelia Gibson",
+      "Cameron Clayton",
+      "Viola Vasquez",
+      "Lula Collins",
+      "Victor Nichols",
+    ],
+    adult: false,
+    image: "https://via.placeholder.com/640/480",
+  },
+];
+
+//? Напишіть функцію getAllTitlesOfFilms(films), яка повертає масив з усіма назвами фільмів.
+
+// const getAllTitlesOfFilms = function (list) {
+//   const arr = [];
+//   for (const el of list) {
+//     arr.push(el.title);
+//   }
+//   return arr;
+// };
+// console.log(getAllTitlesOfFilms(filmoteka));
+
+// //? Напишіть функцію findFilmByName(films, filmTitle), яка шукає фільм за назвою.
+
+function findFilmByName(films, filmTitle) {
+  for (const el of films) {
+    if (el.title === filmTitle) return el;
+  }
+  return `${filmTitle} not find`;
+}
+
+console.log(findFilmByName(filmoteka, "ahdtszh"));

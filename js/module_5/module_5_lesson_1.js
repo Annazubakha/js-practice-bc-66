c4b344000a687bde7f4fac0a92a550485486271d
Sergey Kabanets <skabanets97@gmail.com>
2023-11-21 14:45:26 +0200

    add task 2

--- js/module_5/module_5_lesson_1.js
+++ js/module_5/module_5_lesson_1.js
@@ -3,7 +3,7 @@
 //   showName() {
 //     console.log(this);
 //     const arrow =() => {
-//      console.log(this)   
+//      console.log(this)
 //     }
 //     arrow()
 //   },
@@ -11,15 +11,48 @@
 
 // user.showName()
 
-const user = {
-    name: "Mango",
-    age: "18",
-}
+// const user = {
+//     name: "Mango",
+//     age: "18",
+// }
 
-function showName(){
-    console.log(this)
-}
+// function showName(){
+//     console.log(this)
+// }
 
-user.showName = showName;
-user.showName();
-// showName.call(user)
+// user.showName = showName;
+// user.showName();
+// // showName.call(user)
+
+/*
+? Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю, розраховує
+? та повертає загальну вартість каменів з таким ім'ям, ціною та кількістю з властивості stones.
+*/
+
+const chopShop = {
+  stones: [
+    { name: 'Emerald', price: 1300, quantity: 4 },
+    { name: 'Diamond', price: 2700, quantity: 3 },
+    { name: 'Sapphire', price: 1400, quantity: 7 },
+    { name: 'Ruby', price: 800, quantity: 2 },
+  ],
+
+  calcTotalPrice(stoneName) {
+    // return this.stones.reduce((totalPrice, { name, price, quantity }) => {
+    //   if (name === stoneName) {
+    //     totalPrice = totalPrice + price * quantity;
+    //   }
+    //   return totalPrice;
+    // }, 0);
+
+    const { price, quantity } = this.stones.find(
+      ({ name }) => name === stoneName,
+    );
+    return price * quantity;
+  },
+};
+
+console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+console.log(chopShop.calcTotalPrice('Ruby')); // 1600

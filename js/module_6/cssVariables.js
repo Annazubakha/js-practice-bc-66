dd85cf7f6f7ca814e1ecbfe965b3bcf55a2806e1
Natali258 <natalya.holyavko@gmail.com>
2023-11-28 15:43:44 +0200

    module7-task-3

--- js/module_6/cssVariables.js
+++ js/module_6/cssVariables.js
@@ -1,10 +1,21 @@
-const inputsList = document.querySelectorAll('input');
-function handleChange(event) {
-  const { name, value, dataset } = event.target;
+// const inputsList = document.querySelectorAll('input');
+// function handleChange(event) {
+//   const { name, value, dataset } = event.target;
+//   const sufix = dataset.sizing || '';
+
+//   document.documentElement.style.setProperty(`--${name}`, value + sufix);
+// }
+// inputsList.forEach(input => {
+//   input.addEventListener('input', handleChange);
+// });
+
+const list = document.querySelector('.js-controls');
+
+const handleChange = evt => {
+  const { name, value, dataset, nodeName } = evt.target;
+  // if (nodeName !== 'INPUT') return;
   const sufix = dataset.sizing || '';
 
   document.documentElement.style.setProperty(`--${name}`, value + sufix);
-}
-inputsList.forEach(input => {
-  input.addEventListener('input', handleChange);
-});
+};
+list.addEventListener('input', handleChange);

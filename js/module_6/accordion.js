18acbd61f895a5eb35785cdedf40644fdc906b90
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-11-28 14:21:31 +0200

    task-1

--- js/module_6/accordion.js
+++ js/module_6/accordion.js
@@ -1,8 +1,16 @@
-const accordionButtonList = document.querySelectorAll('.js-accordion-btn');
+// const accordionButtonList = document.querySelectorAll('.js-accordion-btn');
 
-const onClickBtn = event => {
-  const { nextElementSibling } = event.target;
-  nextElementSibling.classList.toggle('active');
-};
+// const onClickBtn = event => {
+//   const { nextElementSibling } = event.target;
+//   nextElementSibling.classList.toggle('active');
+// };
 
-accordionButtonList.forEach(item => item.addEventListener('click', onClickBtn));
+// accordionButtonList.forEach(item => item.addEventListener('click', onClickBtn));
+const list = document.querySelector('.js-accordion-list')
+const handlClick = (event)=>{
+  const { nextElementSibling,nodeName } = event.target;
+  if(nodeName === 'BUTTON'){
+    nextElementSibling.classList.toggle('active');
+  }
+}
+list.addEventListener('click',handlClick)
